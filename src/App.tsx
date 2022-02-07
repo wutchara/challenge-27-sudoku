fc8bdc50fec7f4fbe1a04cf821a9e273af46ca38
wutchara <sstmuham@gmail.com>
2022-02-07 17:38:00 +0700

    Add MUI example into project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }: any) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +31,22 @@ function App() {
           Learn React
         </a>
       </header>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={8}>
+            <Item>xs=6 md=8</Item>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Item>xs=6 md=4</Item>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Item>xs=6 md=4</Item>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <Item>xs=6 md=8</Item>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
