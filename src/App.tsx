d6a2b2e3e81a3871499c32e81404028f04926906
wutchara <sstmuham@gmail.com>
2022-02-12 22:05:02 +0700

    Add handle clear state and add all number in states

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import './App.css';
 
 import SudokuTable from './components/table';
 
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+
 
 function App() {
   return (
@@ -14,15 +13,6 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <SudokuTable title='Sudoku - Challenge' />
-        
-        <Grid container spacing={4}>
-          <Grid item xs={4}>
-            <Button variant="contained" fullWidth color='success'>Resolve</Button>
-          </Grid>
-          <Grid item xs={8}>
-            <Button variant="outlined" fullWidth color='error'>Clear</Button>
-          </Grid>
-        </Grid>
       </header>
     </div>
   );
--- src/components/table.tsx
+++ src/components/table.tsx
@@ -4,6 +4,9 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import LinearProgress from '@mui/material/LinearProgress';
+import Stack from '@mui/material/Stack';
+import Fade from '@mui/material/Fade';
 
 interface TitleProps {
   title: string;
@@ -18,6 +21,7 @@ interface TitleProps {
 interface IState {
   value?: number;// TODO: for test only
   tableValues: string[][][][];
+  loading: boolean;
 }
 
 interface ITableValue {
@@ -31,15 +35,82 @@ class SudokuTable extends Component<TitleProps, IState> {
   constructor(props: TitleProps) {
     super(props);
 
-    this.state = {
-      tableValues: [
-        [
-        [['0', '0', '0'],
-        ['0', '0', '0'],
-        ['0', '0', '0']]
-        ]
-    ],
+    this.state = this.resetState();
+  }
+
+  private resetState() {
+    // create table values
+    const tableValues = [];
+    for (let row = 0; row < 3; row++) {
+      const tableValue = [];
+      for (let col = 0; col < 3; col++) {
+        const tableValueRow = [];
+        for (let col = 0; col < 3; col++) {
+          tableValueRow.push(['', '', '']);
+        }
+        tableValue.push(tableValueRow);
+      }
+      tableValues.push(tableValue);
+    }
+    console.log('tableValues', tableValues);
+
+    return {
+      tableValues,
+    //   [
+    //     [
+    //       [
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //       ],
+    //       [
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //       ],
+    //       [
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //       ],
+    //     ],
+    //     [
+    //       [
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //       ],
+    //       [
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //       ],
+    //       [
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //       ],
+    //     ],
+    //     [
+    //       [
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //       ],
+    //       [
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //       ],
+    //       [
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //         ['0', '0', '0'],
+    //       ],
+    //     ],
+    // ],
       value: 0,
+      loading: true,
     };
   }
 
@@ -58,11 +129,20 @@ class SudokuTable extends Component<TitleProps, IState> {
       };
     };
 
-    // TODO: set the real state
+    // set the real state
     const setNewValue = (value: number, config: ITableValue) => {
       const tableValues = JSON.parse(JSON.stringify(this.state.tableValues)); // clone
-      tableValues[config.rowNumber - 1][config.blogNumber - 1][config.innerX][config.innerY] = value + '';
+      const rowIndex = config.rowNumber - 1;
+      const blogIndex = config.blogNumber - 1;
+      tableValues[rowIndex][blogIndex][config.innerX][config.innerY] = value + '';
       console.log('tableValues', tableValues);
+      this.setState({
+        ...this.state,
+        tableValues,
+      });
+      console.log('rowIndex', rowIndex);
+      console.log('blogIndex', blogIndex);
+      console.log('tableValues[rowIndex][blogIndex]', tableValues[rowIndex][blogIndex]);
     }
 
     const onCHangeState = (value: any) => {
@@ -70,21 +150,18 @@ class SudokuTable extends Component<TitleProps, IState> {
       console.log(value.target.value, typeof value.target.value);
       const newValue = Number(value.target.value);
 
+      // TODO: display error
       // [1, 9] => 10 > value > 1
       if (!isNaN(newValue) && newValue > 0 && newValue < 10) {
         console.log('newValue', newValue);
         setNewValue(newValue, getIdObject( value.target.id));
-        // this.setState({
-        //   ...this.state,
-        //   tableValues: [value.target.value],
-        // });
       }
     };
 
     return (<Box key={'small-box-' + id}>
       <TextField
         focused
-        value={(rowNumber === 1 && blogNumber === 1) ? this.state.tableValues[rowNumber - 1][blogNumber - 1][innerX][innerY] : ''}
+        value={this.state.tableValues[rowNumber - 1][blogNumber - 1][innerX][innerY]}
         id={id}
         // type="number"
         color='warning'
@@ -140,6 +217,33 @@ class SudokuTable extends Component<TitleProps, IState> {
     </Grid>);
   };
 
+  private handleClearBtn(event: any) {
+    console.log('handleClearBtn', event);
+    this.setState(this.resetState());
+    this.setLoadingDelay();
+  }
+
+  private handleResolveBtn(event: any) {
+    console.log('handleResolveBtn', event);
+  }
+
+  private setLoadingState(isLoading: boolean) {
+    this.setState({
+      ...this.state,
+      loading: isLoading,
+    });
+  }
+
+  private setLoadingDelay() {
+    setTimeout(() => {
+      this.setLoadingState(!this.state.loading);
+    }, 3000); // wait 3 seconds
+  }
+
+  componentDidMount() {
+    this.setLoadingDelay();
+  }
+
   render() {
     const { title } = this.props;
 
@@ -156,10 +260,31 @@ class SudokuTable extends Component<TitleProps, IState> {
         >
           Set another value!: { this.state.value }
         </Button>
+        <Fade
+          in={this.state.loading}
+          style={{
+            transitionDelay: this.state.loading ? '800ms' : '0ms',
+          }}
+          unmountOnExit
+        >
+          <Stack sx={{ width: '90%', color: 'grey.500' }} spacing={2}>
+            <LinearProgress color="secondary" />
+          </Stack>
+        </Fade>
+        
+        
         <Grid container spacing={0} margin={2} padding={2}>
           {this.createOuterRow(1)}
           {this.createOuterRow(2)}
           {this.createOuterRow(3)}
+        </Grid>
+        <Grid container spacing={4}>
+          <Grid item xs={4}>
+            <Button variant="contained" fullWidth color='success' onClick={this.handleResolveBtn}>Resolve</Button>
+          </Grid>
+          <Grid item xs={8}>
+            <Button variant="outlined" fullWidth color='error' onClick={(e) => this.handleClearBtn(e)}>Clear</Button>
+          </Grid>
         </Grid>
       </>
     );
